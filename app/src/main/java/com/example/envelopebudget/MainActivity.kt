886276349d0c2638c3610e302775a35937f5fe3e
Matheus Iser <matheuskiser@gmail.com>
2022-05-02 16:23:56 -0700

    Adds design system to app with Material 3

--- app/src/main/java/com/example/envelopebudget/MainActivity.kt
+++ app/src/main/java/com/example/envelopebudget/MainActivity.kt
@@ -1,12 +1,13 @@
 package com.example.envelopebudget
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,7 +21,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
                 }
@@ -34,10 +35,22 @@ fun Greeting(name: String) {
     Text(text = "Hello there 2, $name!")
 }
 
-@Preview(showBackground = true)
+@Preview(
+    name = "Night Mode - Empty",
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+)
+@Preview(
+    name = "Day Mode - Empty",
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+)
 @Composable
-fun DefaultPreview() {
+private fun LoginContentPreview() {
     EnvelopeBudgetTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Greeting("Android")
+        }
     }
 }
--- app/src/main/java/com/example/envelopebudget/ui/theme/Theme.kt
+++ app/src/main/java/com/example/envelopebudget/ui/theme/Theme.kt
@@ -1,47 +1,23 @@
 package com.example.envelopebudget.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
-
-private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
-)
 
 @Composable
 fun EnvelopeBudgetTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    useDarkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
+    val colors = if (!useDarkTheme) {
+        LightThemeColors
     } else {
-        LightColorPalette
+        DarkThemeColors
     }
 
     MaterialTheme(
-        colors = colors,
-        typography = Typography,
-        shapes = Shapes,
+        colorScheme = colors,
+        typography = AppTypography,
         content = content
     )
 }
